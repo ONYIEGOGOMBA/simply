6b56ec176583a7b60590f2b279dfabe9b38878a7
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-10 04:42:31 -0700

    betty must be proud

---
+++ mary_brian_betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints us.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("MARY MUTHONI AND BRIAN GOMBA DID THIS TASK,\n");
+	printf("AUGUST 2023.\n");
+
+	return (0);
+}
