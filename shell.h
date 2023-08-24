c6d1e5190b5383d9bb71eb46b7361aa20abaf7c7
ONYIEGOGOMBA <briangomba463@gmail.com>
2023-08-24 06:26:48 -0700

    changes

--- shell.h
+++ shell.h
@@ -15,5 +15,9 @@ void parseArguments(char *input, char **args);
 void display_prompt(char **user_input, size_t *input_size);
 void execute_prompt(char *command);
 
+int _myhelp(ino_t *info);
+int _mycd(ino_t *info);
+int _myexit(ino_t *info);
+
 
 #endif
